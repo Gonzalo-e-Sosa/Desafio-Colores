2a451a658af5a934a148b14c9794bb4b239f35c1
Gonza <gonzalososa342@gmail.com>
2024-08-06 20:23:08 -0300

    Util functions

---
+++ frontend/src/utils/func.ts
@@ -0,0 +1,15 @@
+export function getRandomHexaNumber(key = 0): string {
+  return Math.floor((Math.random() + key) * 0xffffff).toString(16).padEnd(6, "0")
+}
+
+export function fromRGBtoHexadecimal(rgb: string): string {
+  return (rgb.split("(")[1].split(")")[0])
+    .split(",")
+    .map(n => {
+      let hex = parseInt(n).toString(16);
+      return hex.length == 1
+        ? "0" + hex
+        : hex
+    })
+    .join("");
+}
