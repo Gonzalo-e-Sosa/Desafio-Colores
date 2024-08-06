76a08561b12c4bc84ce529c0fe38b7e2e24975da
Gonza <gonzalososa342@gmail.com>
2024-08-06 20:21:23 -0300

    Option component to render a random color

---
+++ frontend/src/components/Option.tsx
@@ -0,0 +1,52 @@
+import './Option.css';
+import type { Level as TLevel } from '../types.d';
+import { getRandomHexaNumber } from '../utils/func';
+
+interface Props {
+  id: number
+  seedColor: string
+  generateRandom: boolean
+  level: TLevel
+  onClick: (e: React.MouseEvent) => void
+}
+
+const Option: React.FC<Props> = ({ id, seedColor, generateRandom, level, onClick }) => {
+  let color = seedColor;
+
+  if (generateRandom) {
+    switch (level) {
+      case 'fácil':
+        color = id % 2 === 0
+          ? getRandomModifiedHexaNumber(seedColor, id)
+          : getRandomHexaNumber();
+        break;
+      case 'normal':
+        color = id % 2 === 0
+          ? getRandomModifiedHexaNumber(seedColor, id)
+          : getRandomHexaNumber(id);
+        break;
+      case 'difícil':
+        color = getRandomModifiedHexaNumber(seedColor, id);
+        break;
+    }
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className="option"
+      style={{ backgroundColor: `#${color}` }}
+    >
+    </button>
+  )
+}
+
+const getRandomModifiedHexaNumber = (seedColor: string, id: number): string => {
+  const randomPos = Math.floor(Math.random() * 6);
+  const randomNum = Math.floor((Math.random() + id) * 0xff);
+  const fractionToChange = seedColor.substring(randomPos, randomPos + 1);
+
+  return seedColor.replace(fractionToChange, randomNum.toString(16));
+};
+
+export default Option;
